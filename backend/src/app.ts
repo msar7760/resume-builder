37f1ba2b7ee48d2bad3a26572be04cb01a55cc65
Yakupitiyage, Mahesh Sarathchandra <mahesh.yakupitiyage@th-bingen.de>
2026-01-09 14:33:59 +0100

    add build script and required changes for the deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { globalErrorHandler } from './middlewares/error.middleware';
 import resumeBuilderRoutes from "./routes/resumeBuilder.route";
+import cors from 'cors';
 
 function createApp() {
     const app = express();
     app.use(express.json());
+    app.use(cors());
     app.use('/api', resumeBuilderRoutes);
     app.use(globalErrorHandler);
 
--- backend/src/controllers/resumeBuilder.controller.ts
+++ backend/src/controllers/resumeBuilder.controller.ts
@@ -4,7 +4,6 @@ import { asyncHandler } from '../utils/async.handler';
 import * as resumeBuilderService from "../services/resumeBuilder.service";
 
 export const createResume = asyncHandler(async (req: Request, res: Response) => {
-    const { jsonContent } = req.body;
-    const buffer = await resumeBuilderService.createResume(jsonContent);
+    const buffer = await resumeBuilderService.createResume(req.body);
     return sendFileSuccess(res, buffer, 'resume.docx');
 });
